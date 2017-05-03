322423876a7b8ca98b10c2a23a5bfaf84535482a
zppro <zpprozppro@gmail.com>
2017-05-03 16:23:56 +0800

    fix robot bed_monitor belong to tenant tree

---
+++ src/vendor-1.x/js/modules/backfiller/backfiller.directive.js
@@ -0,0 +1,110 @@
+/**
+ * utils.directive Created by zppro on 16-3-24.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('app.backfill')
+        .directive('backfill', backfill)
+    ;
+
+    sDropdown.$inject = ['$q', '$timeout'];
+    function sDropdown($q, $timeout) {
+
+
+        var directive = {
+            restrict: 'EA',
+            templateUrl: function (elem, attrs) {
+                return attrs.dropdownTemplateUrl || 'dropdown-default-renderer.html';
+            },
+            link: link,
+            scope: {sDropdownData: '=', onSelect: '&', model: '=ngModel', emptyPlaceholder: '='}
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+
+            var data = scope.sDropdownData;
+            if (!data) {
+                return;
+            }
+            var option = scope.$eval(attrs.sDropdownOption) || {};
+            var selectItemFormat = option.selectItemFormat || 'value';
+            var valueKey = scope.valueKey = option.valueKey || 'value';
+            var textKey =  scope.textKey = option.textKey || 'name';
+
+            scope.$watch("sDropdownData",function(newValue,oldValue) {
+                if (newValue != oldValue) {
+                    $timeout(function () {
+                        $q.when(newValue).then(function (items) {
+                            scope.items = items;
+                            setShowText();
+                        });
+                        console.log('dropdown recreate end')
+                    }, 0);
+                }
+            });
+
+            // Bring in changes from outside:
+            scope.$watch('model', function(newValue,oldValue) {
+                // console.log('model newValue:',newValue ,' oldValue:', oldValue);
+                if (newValue != oldValue) {
+                    scope.$eval(attrs.ngModel + ' = model');
+                    setShowText();
+                }
+            });
+
+            // Send out changes from inside:
+            //scope.$watch(attrs.ngModel, function(val) {
+            //    scope.model = val;
+            //});
+
+
+
+            scope.isButton = 'isButton' in attrs;
+
+            element.on('click', function (event) {
+                event.preventDefault();
+            });
+
+            scope.select = function (item) {
+                scope.model = selectItemFormat == 'object' ? item : item[valueKey];
+                if (scope.onSelect) {
+                    $timeout(function () {
+                        scope.onSelect({item: item});
+                    }, 0);
+                }
+            };
+            $q.when(data).then(function (items) {
+                scope.items = items;
+                setShowText();
+            });
+
+
+
+            function setShowText() {
+                scope.showText = scope.emptyPlaceholder || '请选择';
+                if (scope.items) {
+                    for (var i = 0; i < scope.items.length; i++) {
+                        if (selectItemFormat == 'object') {
+                            if (scope.model && scope.items[i] == scope.model) { //|| (valueKey && scope.items[i][valueKey] == scope.model[valueKey])
+                                scope.showText = scope.items[i][textKey];
+                                break;
+                            }
+                        }
+                        else {
+
+                            if (scope.items[i][valueKey] == scope.model) {
+                                scope.showText = scope.items[i][textKey];
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+})();

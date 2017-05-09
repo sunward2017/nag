7b026f16488ce03ab610ac86e4bf98e17f2ea04c
zppro <zpprozppro@gmail.com>
2017-05-09 17:23:05 +0800

    regenerate by components

---
+++ src/jobs/nursingRecordInfoUpdate.js
@@ -0,0 +1,49 @@
+/**
+ * Created by zppro on 17-5-9.
+ * Target:护理记录数据更新 http://nodeclass.com/articles/78767
+ */
+ var co = require('co');
+ var schedule = require('node-schedule');
+
+ var job_id = 'nursingRecordInfoUpdate';
+ var job_name =  '护理记录数据更新';
+ var job_rule = '1 0 * * *';//每天 几点:几分
+ var printLog = true;
+
+ module.exports = {
+     needRegister: true,
+     register: function (ctx) {
+         if (this.needRegister) {
+             return co(function*() {
+                 ctx.jobManger.createJob(job_id, job_name, job_rule, ()=> {
+                     console.log(ctx.moment().format('HH:mm:ss') + ' ' + job_id + '(' + job_name + ') => executing.');
+                     if (ctx.onJobExecute && ctx._.isFunction(ctx.onJobExecute)) {
+                         ctx.onJobExecute.call(null, job_id);
+                     }
+
+                     //归档昨天的护理记录
+                     ctx.app_archive_service.archivePSN$NursingRecord();
+
+                     //生成新一天的护理记录
+                     // console.log(ctx.moment().format('HH:mm:ss') + ' ' + job_id + '(' + job_name + ') => executed.');
+                 }, {printLog: printLog});
+
+                 return {
+                     success: true,
+                     job_id: job_id,
+                     job_name: job_name,
+                     job_rule: job_rule
+                 }
+             }).catch(ctx.coOnError);
+         }
+         else {
+             console.log(job_id + '(' + job_name + ') => skip register.');
+             return Promise.resolve({
+                 success: false,
+                 job_id: job_id,
+                 job_name: job_name,
+                 job_rule: job_rule
+             });
+         }
+     }
+ }

e0443a20eb10b423fe98dbaa1decd96357bbfbda
ahcl <ahcl4545@163.com>
2017-12-05 11:44:48 +0800

    增加elderly\meal\meal-dish页py字段批量生成按钮

--- src/vendor-1.x/js/lazy-modules/pension-agency/meal-dish.js
+++ src/vendor-1.x/js/lazy-modules/pension-agency/meal-dish.js
@@ -22,7 +22,14 @@
 
         function init() {
             vm.init({removeDialog: ngDialog});
+            vm.batchCreatePy = batchCreatePy;
             vm.query();
+        }
+
+        function batchCreatePy() {
+          vmh.psnService.batchCreatePy(vm.tenantId,'psn_mealDish','name').then(function () {
+            vmh.alertSuccess();
+          });
         }
     }
 
